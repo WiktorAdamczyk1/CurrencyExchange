a750ba3f620d1d7d44296a9092ccb947ad2620e4
Wiktor Adamczyk <67286353+WiktorAdamczyk1@users.noreply.github.com>
2021-10-08 19:33:57 +0200

    Add project files.

---
+++ CurrencyExchange/Data/Currency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyExchange.Data
+{
+    public class Currency
+    {
+
+        string Name { get; set; }
+        decimal ExchangeRate { get; set; }
+        int Value { get; set; }
+        int Unit { get; set; }
+        int Amount { get; set; }
+    }
+}
